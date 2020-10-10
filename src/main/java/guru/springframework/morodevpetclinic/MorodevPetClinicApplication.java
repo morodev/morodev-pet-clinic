be285ed696c3ea28f57229051a9256844ba25a24
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-10-10 18:07:33 +0200

    Initial Project Add Pet Clinic

---
+++ src/main/java/guru/springframework/morodevpetclinic/MorodevPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.morodevpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorodevPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorodevPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/morodevpetclinic/MorodevPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.morodevpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorodevPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
