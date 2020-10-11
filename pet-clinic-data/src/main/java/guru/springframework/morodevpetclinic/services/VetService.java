1e15ded8cfbd932ad3a1de7490519bb8f561a354
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-10-11 19:20:22 +0200

    Implement Interfaces for services

---
+++ pet-clinic-data/src/main/java/guru/springframework/morodevpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.morodevpetclinic.services;
+
+import guru.springframework.morodevpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
