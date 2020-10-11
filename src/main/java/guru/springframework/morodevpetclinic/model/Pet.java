baa84c7ab0006a30cee51ea33a0ed69e432dc464
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-10-11 15:32:42 +0200

    adding POJOs.

---
+++ src/main/java/guru/springframework/morodevpetclinic/model/Pet.java
@@ -0,0 +1,37 @@
+package guru.springframework.morodevpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Luca Moro on 11/10/2020 15:28
+ */
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
---
+++ src/main/java/guru/springframework/morodevpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package guru.springframework.morodevpetclinic.model;
+
+/**
+ * Created by Luca Moro on 11/10/2020 15:30
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
