0de734287bfb3b6e8e657a411a915a483a9aa24c
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-10-25 16:09:26 +0100

    added Visits, update Owner with Pet relationship. Closes #9

--- pet-clinic-data/src/main/java/guru/springframework/morodevpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/morodevpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.morodevpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Luca Moro on 11/10/2020 15:26
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
