f9e1c583523d5307d2d228be63fb643e34d8b46d
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-10-31 11:58:59 +0100

    Adding Visit JPA Entity.

--- pet-clinic-data/src/main/java/guru/springframework/morodevpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/morodevpetclinic/model/Visit.java
@@ -2,6 +2,7 @@ package guru.springframework.morodevpetclinic.model;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
@@ -12,10 +13,18 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
