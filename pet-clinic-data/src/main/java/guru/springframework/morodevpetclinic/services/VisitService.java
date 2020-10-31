5f7281683bfba028a1e10ea418b3cc00c90b367c
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-10-31 14:25:51 +0100

    - add Visit Service - Implement Map Impl for Visit Service closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/morodevpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package guru.springframework.morodevpetclinic.services;
+
+import guru.springframework.morodevpetclinic.model.Visit;
+
+/**
+ * Created By Luca Moro on 31/10/2020 14:14
+ */
+public interface VisitService extends CrudService<Visit, Long>{
+}
